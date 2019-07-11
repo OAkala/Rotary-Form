03b0d4dfbdd8749fec2b3325f0b5dfc5ae8094bb
OAkala <olalekan56@gmail.com>
2019-07-10 20:37:41 -0400

    removed unused field and and edited some methods

--- RotaryFormRecord.java
+++ RotaryFormRecord.java
@@ -10,10 +10,10 @@
 */
 
 import java.lang.reflect.*;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class RotaryFormRecord extends PestProblem {
-    public static final int[] STRING_FIELDS = {1, 2, 3, 5, 6, 7};
     private String unitAddress;
     private String keyHome;
     private boolean followup;
@@ -22,9 +22,15 @@ public class RotaryFormRecord extends PestProblem {
     private String comments;
 
     public RotaryFormRecord() {
-
+        comments = "none";
     }
 
+    /**
+     * @param unitAddress
+     * @param keyHome
+     * @param pestLevel
+     * @param houseKeeping
+     */
     public RotaryFormRecord(String unitAddress, String keyHome, String pestLevel,
                             String houseKeeping) {
         setUnitAddress(unitAddress);
@@ -33,10 +39,17 @@ public class RotaryFormRecord extends PestProblem {
         setHouseKeeping(houseKeeping);
     }
 
+    /**
+     * @return
+     */
     public String getUnitAddress() {
         return unitAddress;
     }
 
+    /**
+     * @param unitAddress
+     * @return
+     */
     public int setUnitAddress(String unitAddress) {
         if (!unitAddress.isEmpty()) {
             this.unitAddress = unitAddress;
@@ -71,6 +84,10 @@ public class RotaryFormRecord extends PestProblem {
         return pestLevel;
     }
 
+    /**
+     * @param pestLevel
+     * @return
+     */
     public int setPestLevel(String pestLevel) {
         if (Stream.of("low", "medium", "high").anyMatch(pestLevel::equalsIgnoreCase)) {
             this.pestLevel = pestLevel;
@@ -84,7 +101,11 @@ public class RotaryFormRecord extends PestProblem {
         return houseKeeping;
     }
 
-    public int setHouseKeeping(String houseKeeping) {
+    /**
+     * @param houseKeeping
+     * @return
+     */
+    private int setHouseKeeping(String houseKeeping) {
         if (!houseKeeping.isEmpty()) {
             this.houseKeeping = houseKeeping;
             return 1;
@@ -94,39 +115,40 @@ public class RotaryFormRecord extends PestProblem {
 
     }
 
+    /**
+     * @return
+     */
     public String getComments() {
         return comments;
     }
 
-    public void setComments(String comments) {
-        this.comments = comments;
+    private int setComments(String comments) {
+        if (!comments.isEmpty()) {
+            this.comments = comments;
+            return 1;
+        }
+        System.out.println("Please enter valid response.");
+        return -1;
     }
 
+    /**
+     * @param field
+     * @param data
+     */
     public void fill(int field, String data) {
-        boolean check = true;
-        for (int n : STRING_FIELDS
-        ) {
-            check = n == field;
-            if (!check) {
-                break;
-            }
-        }
-
-        if (check) {
-            switch (field) {
-                case 1:
-                    setUnitAddress(data);
-                case 2:
-                    setOthers(data);
-                case 3:
-                    setKeyHome(data);
-                case 5:
-                    setPestLevel(data);
-                case 6:
-                    setHouseKeeping(data);
-                case 7:
-                    setComments(data);
-            }
+        switch (field) {
+            case 1:
+                setUnitAddress(data);
+            case 2:
+                setOthers(data);
+            case 3:
+                setKeyHome(data);
+            case 5:
+                setPestLevel(data);
+            case 6:
+                setHouseKeeping(data);
+            case 7:
+                setComments(data);
         }
     }
 
@@ -136,35 +158,37 @@ public class RotaryFormRecord extends PestProblem {
         }
     }
 
-    public String toString(Object o) throws Exception {
+    /**
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    String toString(Object o) throws Exception {
         if (o instanceof RotaryFormRecord) {
             StringBuilder str = new StringBuilder();
 
-            for (Field field : getClass().getDeclaredFields()
-            ) {
+            for (Field field : getClass().getDeclaredFields()) {
                 String value = null;
-                if (!field.getName().equals("STRING_FIELDS")) {
-                    StringBuilder getter = new StringBuilder();
-                    if (field.getName().equals("followup")) {
-                        getter.append("is");
-                    } else {
-                        getter.append("get");
-                    }
-                    getter.append(field.getName().substring(0, 1).toUpperCase());
-                    getter.append(field.getName().substring(1));
-                    try {
-                        Method method = getClass().getDeclaredMethod(getter.toString());
-                        value = String.valueOf(method.invoke(o));
-                    } catch (NoSuchMethodException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                    str.append(value);
-                    str.append(",");
+                StringBuilder getter = new StringBuilder();
+                if (field.getName().equals("followup")) { //modify this
+                    getter.append("is");
+                } else {
+                    getter.append("get");
                 }
+                getter.append(field.getName().substring(0, 1).toUpperCase());
+                getter.append(field.getName().substring(1));
+                try {
+                    Method method = getClass().getDeclaredMethod(getter.toString());
+                    value = String.valueOf(method.invoke(o));
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+                str.append(value);
+                str.append(",");
             }
             str.append(toString());
             return str.toString();
