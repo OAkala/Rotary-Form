1958a75dc317977ec03143b8689190d36bdff651
OAkala <olalekan56@gmail.com>
2019-07-15 22:31:10 -0400

    Created client program

---
+++ RotaryFormClient.java
@@ -0,0 +1,73 @@
+/*
+   *************************************************************************************************
+   Program: RotaryFormClient.java
+   Repository Name: Olalekan AKala
+   Date Created: 15-Jul-19
+   Program Description: This is the program the user would interact with to create the pdf forms
+   using the software
+   *************************************************************************************************
+*/
+import java.util.*;
+
+public class RotaryFormClient {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        getValidMenuChoice(scanner, displayMainMenu());
+    }
+
+    public static int displayMainMenu() {
+        System.out.println("\nROTARY FORM MENU\n");
+        System.out.println("N - New Form");
+        System.out.println("O - Open Form");
+        System.out.println("Q - Quit");
+        return 0;
+    }
+
+    public static char getValidMenuChoice(Scanner scnr, int menu) throws Exception {
+        switch (menu) {
+            case 0:
+                // the loop runs until the user enters a valid menu option
+                while (true) {
+                    System.out.print("\nChoose an option: ");
+
+                    String userChoice = scnr.next();
+                    if (userChoice.equalsIgnoreCase("n")) {
+                        return 'n';
+                    } else if (userChoice.equalsIgnoreCase("o")) {
+                        return 'o';
+                    } else if (userChoice.equalsIgnoreCase("q")) {
+                        return 'q';
+                    }
+                    System.out.println("Please choose a valid option");
+                }
+            case 1:
+//                while (true) {
+//                    System.out.print("\nChoose an option: ");
+//
+//                    String userChoice = scnr.next();
+//                    if (userChoice.equalsIgnoreCase("a")) {
+//                        return 'a';
+//                    } else if (userChoice.equalsIgnoreCase("d")) {
+//                        return 'd';
+//                    } else if (userChoice.equalsIgnoreCase("q")) {
+//                        return 'q';
+//                    }
+//                    System.out.println("Please choose a valid option");
+//                }
+        }
+        throw new Exception("Invalid Menu Number");
+    }
+
+    public static int displaySubMenu() {
+        System.out.println("\nPLEASE CHOOSE AN OPERATION\n");
+        System.out.println("A - Add a new record");
+        System.out.println("F - Fill record");
+        System.out.println("E - Edit record");
+        System.out.println("C - Clear record");
+        System.out.println("D - Delete record");
+        System.out.println("R - Reset form");
+        System.out.println("S - Submit form");
+        System.out.println("Q - Quit");
+        return 0;
+    }
+}
